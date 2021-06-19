da44738dae60807d6406803ef6b6c1ff1a6a7bb0
SURISANDY <86154611+SURISANDY@users.noreply.github.com>
2021-06-19 18:37:47 +0530

    Added add method

--- Calculate.java
+++ Calculate.java
@@ -2,4 +2,9 @@ public class Calculate{
   public static void main(String args[]){
     System.out.println("This is from Calculator Program");
   }
+  
+  public void add(int a, int b){
+    System.out.println("Addition of a + b is : " +(a+b));
+  }
+  
 }
