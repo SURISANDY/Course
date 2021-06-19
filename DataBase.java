d7b06478920b9f5099391020f6b3fbc5cf3669f4
PraveenDarling <86154871+PraveenDarling@users.noreply.github.com>
2021-06-19 19:37:52 +0530

    added Integer file

---
+++ DataBase.java
@@ -0,0 +1,14 @@
+public class DataTypes
+{
+ public static void main(String[] args)
+ {
+	 int minIntValue = Integer.MIN_VALUE;
+	 int maxIntValue = Integer.MAX_VALUE;
+	 
+	 System.out.println("INTEGER MINIMUM VALUE = " +  minIntValue);
+	 System.out.println("INTEGER MAXMINM VALUE = " +  maxIntValue);
+	  System.out.println(" ");
+	 System.out.println(" ");
+	 
+ }
+}
